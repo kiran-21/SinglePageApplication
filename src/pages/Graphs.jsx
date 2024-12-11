e78fc583e00cb0ac32c11db798a5e06d495942bb
kiran-21 <kiranksr.sr@gmail.com>
2024-12-11 17:30:18 -0500

    UI changed

--- src/pages/Graphs.jsx
+++ src/pages/Graphs.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const Graphs =()=>{
     return(
-        <div>
-            this is Graph page showing the task in Pie chart or another chart quick review
-        </div>
+        <Box
+        sx={{
+          minHeight: "80vh", 
+          padding: "20px",
+          display: "flex", 
+          flexDirection: "column", 
+          alignItems: "center", 
+          justifyContent: "flex-start", 
+          backgroundColor: "#f5f5f5", 
+        }}
+      >
+        <Typography variant="h5" sx={{ marginBottom: "20px" }}>
+          Graph Overview
+        </Typography>
+        <Typography variant="body1">
+          This is the Graph page showing tasks in a Pie chart or another chart for a quick review.
+        </Typography>
+          <Typography variant="body2" color="textSecondary">
+            Chart will be displayed here
+          </Typography>
+      </Box>
+  
     )
 }
 export default Graphs;
